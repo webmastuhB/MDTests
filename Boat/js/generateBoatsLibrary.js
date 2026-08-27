441eab7f0ed1b13921ff4852519e0ed6994b4811
WebmastuhB <brian1701a@gmail.com>
2026-08-27 05:57:39 -0500

    added boat

---
+++ Boat/js/generateBoatsLibrary.js
@@ -0,0 +1,443 @@
+﻿const arrayOfBoatManufacturers = [
+    'AB Inflatables', 
+    'Action Craft', 
+    'Alumacraft', 
+    'Alweld', 
+    'Arc Boats', 
+    'Armada', 
+    'Ascend', 
+    'Aspen Power Catamarans', 
+    'ATX Surf Boats', 
+    'Avalon Pontoons', 
+    'Avid Boats', 
+    'Axis Wake', 
+    'Back Cove Yachts', 
+    'Balise', 
+    'Barletta Boats', 
+    'Bass Cat', 
+    'Bayliner', 
+    'Bennington', 
+    'Bentley Pontoons', 
+    'Berkshire Pontoons', 
+    'Bertram', 
+    'Blackfin', 
+    'BlackJack Boats', 
+    'Blue Wave Boats', 
+    'Boston Whaler', 
+    'Brunswick Commercial', 
+    'Bullet Boats', 
+    'Bulls Bay', 
+    'Burger Boat Company', 
+    'Cape Craft', 
+    'Cape Horn', 
+    'Carolina Skiff', 
+    'Caymas Boats', 
+    'Centurion Boats', 
+    'Century Boats', 
+    'Chaparral', 
+    'Chris-Craft', 
+    'Cigarette Racing', 
+    'Cobalt Boats', 
+    'Cobia', 
+    'Contender Boats', 
+    'Crest Pontoons', 
+    'Crestliner', 
+    'Crevalle Boats', 
+    'Crownline', 
+    'Cruisers Yachts', 
+    'Cutwater Boats', 
+    'Cypress Cay', 
+    'DCB Performance Marine', 
+    'Donzi Marine', 
+    'Duckworth', 
+    'EdgeWater', 
+    'Egret Boats', 
+    'Eliminator Boats', 
+    'Everglades Boats', 
+    'Excel Boats', 
+    'Falcon Boats', 
+    'Fish Rite', 
+    'Fleming Yachts', 
+    'FLOE Craft', 
+    'Formula', 
+    'Fountain Powerboats', 
+    'Four Winns', 
+    'Freeman Boatworks', 
+    'Frontier Boats', 
+    'G3 Boats', 
+    'Gator Trax Boats', 
+    'Glastron', 
+    'Godfrey Pontoon Boats', 
+    'Grady-White', 
+    'Grumman Boats', 
+    'Harris', 
+    'Havoc Boats', 
+    'HCB Yachts', 
+    'Hewes Boats', 
+    'Hewescraft', 
+    'Heyday', 
+    'Hinckley', 
+    'Hurricane', 
+    'Hustler Powerboats', 
+    'iKon Boats', 
+    'Intrepid Powerboats', 
+    'Invincible Boats', 
+    'Island Packet Yachts', 
+    'J Boats', 
+    'Jupiter Marine', 
+    'Katanacraft', 
+    'Key West Boats', 
+    'KingFisher Boats', 
+    'Landau Boats', 
+    'Legend Boats', 
+    'Little River Marine', 
+    'Lowe Boats', 
+    'Lund Boats', 
+    'MAKO', 
+    'Malibu Boats', 
+    'Manitou Pontoon Boats', 
+    'Marlow Yachts', 
+    'MasterCraft', 
+    'Maverick', 
+    'MB Sports', 
+    'Meyers Boat Company / Sportspal', 
+    'Midnight Express', 
+    'MirroCraft', 
+    'Montara Boats', 
+    'Montego Bay Pontoons', 
+    'Monterey Boats', 
+    'Moomba', 
+    'MTI (Marine Technology Inc.)', 
+    'Mystic Powerboats', 
+    'NauticStar', 
+    'Nautique', 
+    'Nitro', 
+    'Nor-Tech', 
+    'North River Boats', 
+    'Old Town', 
+    'Outer Reef Yachts', 
+    'Parker Boats', 
+    'Pathfinder', 
+    'Pavati', 
+    'Pelican', 
+    'Phenom Yachts', 
+    'Phoenix Bass Boats', 
+    'Polar Kraft', 
+    'Premier Pontoons', 
+    'Princecraft', 
+    'Pursuit', 
+    'Qwest Pontoons', 
+    'Ranger Boats', 
+    'Ranger Tugs', 
+    'Reflex', 
+    'Regal', 
+    'Regency', 
+    'Regulator', 
+    'Robalo', 
+    'Rogue Jet Boatworks', 
+    'Sabre Yachts', 
+    'Sailfish Boats', 
+    'Sanger Boats', 
+    'Sanpan', 
+    'Scarab', 
+    'Scout Boats', 
+    'Sea Born', 
+    'Sea Chaser', 
+    'Sea Eagle', 
+    'Sea Fox', 
+    'Sea Hunt', 
+    'Sea Pro', 
+    'Sea Ray', 
+    'Sea-Doo', 
+    'SeaArk', 
+    'SeaHunter', 
+    'SeaVee Boats', 
+    'Shallow Sport', 
+    'Skater Powerboats', 
+    'Skeeter', 
+    'Smoker Craft', 
+    'Solace Boats', 
+    'Solara', 
+    'South Bay Pontoons', 
+    'Southport Boats', 
+    'Sportsman Boats', 
+    'Starcraft Marine', 
+    'Stingray Boats', 
+    'Sun Tracker', 
+    'SunCatcher Pontoons', 
+    'SunChaser', 
+    'Sunsation Powerboats', 
+    'Supra', 
+    'Supreme Boats', 
+    'Sweetwater', 
+    'Sylvan Marine', 
+    'Tahoe Boats', 
+    'Tahoe Pontoons', 
+    'Thunder Jet', 
+    'Tiara Yachts', 
+    'Tidewater Boats', 
+    'Tige', 
+    'Tracker Boats', 
+    'Trifecta Pontoons', 
+    'Triton Boats', 
+    'Twin Vee', 
+    'Valhalla Boatworks', 
+    'Varatti Boats', 
+    'Veranda', 
+    'Vexus Boats', 
+    'Viaggio Pontoons', 
+    'Viking Yachts', 
+    'Walker Bay', 
+    'War Eagle Boats', 
+    'WeldBilt', 
+    'Weldcraft', 
+    'Wellcraft', 
+    'Wenonah Canoe', 
+    'World Cat', 
+    'Xpress Boats', 
+    'Yamaha Boats', 
+    'Yar-Craft', 
+    'Yellowfin', 
+    'Zodiac Nautic'
+];
+
+
+const arrayOfBoatMakes = [
+    'Montauk 170',
+    'Montauk 190',
+    'Montauk 210',
+    'Outrage 230',
+    'Outrage 250',
+    'Outrage 280',
+    'Outrage 330',
+    'Dauntless 220',
+    'Dauntless 250',
+    'Vantage 240',
+    'Vantage 280',
+    'Element E16',
+    'Element E18',
+    'VR4',
+    'VR5',
+    'VR6',
+    'DX2000',
+    'DX2200',
+    'SPX 190',
+    'SPX 210',
+    'SDX 250',
+    'SDX 270',
+    'SLX 260',
+    'SLX 280',
+    'Sundancer 320',
+    'Sundancer 370',
+    'Super Air Nautique G23',
+    'Super Air Nautique G25',
+    'Paragon G23',
+    'Paragon G25',
+    'Wakesetter 21 LX',
+    'Wakesetter 22 LSV',
+    'Wakesetter 23 LSV',
+    'Wakesetter 24 MXZ',
+    'Wakesetter 25 LSV',
+    'X22',
+    'X24',
+    'X26',
+    'XT20',
+    'XT22',
+    'XT23',
+    'T220',
+    'T235',
+    'T250',
+    '23 LSV',
+    '24RZX',
+    'Z3',
+    'ZX22',
+    'ZX25',
+    'Mojo',
+    'Mondo',
+    'Makaira',
+    'Craz',
+    'Reata 220F',
+    'Reata 243C',
+    'Ranger Z518',
+    'Ranger Z519',
+    'Ranger Z520',
+    'Ranger RT178',
+    'Ranger RT188',
+    'Z18',
+    'Z19',
+    'Z20',
+    'Z21',
+    'Pro Team 175 TXW',
+    'Pro Team 190 TX',
+    'Pro Team 195 TXW',
+    'Targa V-18',
+    'Targa V-19',
+    'Targa V-20',
+    'Classic XL',
+    '1650 Rebel XL',
+    '1775 Adventure',
+    '1875 Impact XS',
+    '1875 Pro-V',
+    '1975 Pro-V',
+    '2075 Pro-V',
+    '1650 Fish Hawk',
+    '1750 Fish Hawk',
+    '1850 Fish Hawk',
+    'MX 18',
+    'MX 19',
+    'Sportsman 1710',
+    'Sportsman 1810',
+    'Sportsman 1910',
+    'Bay Reef 210',
+    'Bay Reef 230',
+    'Bay Reef 250',
+    'Sportsman 212',
+    'Sportsman 232',
+    'Ultra 219',
+    'Ultra 239',
+    'Gamefish 25',
+    'Gamefish 27',
+    'Gamefish 30',
+    'Fisherman 236',
+    'Fisherman 257',
+    'Fisherman 262',
+    'Fisherman 302', 
+    'Fisherman 352'
+];
+
+
+function generateUniqueIDs(count = 20) {
+    const numbers = new Set();
+
+    while (numbers.size < count) {
+        const value = Math.floor(Math.random() * 9000) + 1000;
+        numbers.add(value);
+    }
+
+    return Array.from(numbers);
+}
+
+function generateLicenseNumbers(count = 20) {
+    const licenseNumbers = new Set();
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    while (licenseNumbers.size < count) {
+        const number = Math.floor(Math.random() * 9000) + 1000;
+
+        const letter1 = letters[Math.floor(Math.random() * letters.length)];
+        const letter2 = letters[Math.floor(Math.random() * letters.length)];
+
+        const licenseNumber = `LA-${number}-${letter1}${letter2}`;
+
+        licenseNumbers.add(licenseNumber);
+    }
+
+    return Array.from(licenseNumbers);
+}
+
+function generateBoatManufacturers(count = 20) {
+    // does not need to be unique
+    const boatManufacturersOut = [];
+    while (boatManufacturersOut.length < count) {
+        const thisMfg = arrayOfBoatManufacturers[Math.floor(Math.random() * arrayOfBoatManufacturers.length)];
+        boatManufacturersOut.push(thisMfg);
+    }
+    return boatManufacturersOut;
+}
+
+function generateBoatMakes(count = 20) {
+    // does not need to be unique
+    const boatMakesOut = [];
+    while (boatMakesOut.length < count) {
+        const thisMake = arrayOfBoatMakes[Math.floor(Math.random() * arrayOfBoatMakes.length)];
+        boatMakesOut.push(thisMake);
+    }
+    return boatMakesOut;
+}
+
+function generateModelYears(count = 20) {
+    // does not need to be unique
+    const boatModelYearsOut = [];
+    while (boatModelYearsOut.length < count) {
+        const thisYear = Math.floor(Math.random() * 25) + 2000;
+        boatModelYearsOut.push(thisYear);
+    }
+    return boatModelYearsOut;
+}
+
+function generateExpDates(count = 20) {
+
+    function randomDateBetween(startDate, endDate) {
+        const start = startDate.getTime();
+        const end = endDate.getTime();
+
+        return new Date(
+            start + Math.random() * (end - start)
+        );
+    }
+
+    const dates = [];
+    const today = new Date();
+
+    // Strip off the time portion
+    today.setHours(0, 0, 0, 0);
+
+    const twoYearsAgo = new Date(today);
+    twoYearsAgo.setFullYear(twoYearsAgo.getFullYear() - 2);
+
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    const sixtyDaysFromNow = new Date(today);
+    sixtyDaysFromNow.setDate(sixtyDaysFromNow.getDate() + 60);
+
+    const sixtyOneDaysFromNow = new Date(today);
+    sixtyOneDaysFromNow.setDate(sixtyOneDaysFromNow.getDate() + 61);
+
+    const twoYearsFromNow = new Date(today);
+    twoYearsFromNow.setFullYear(twoYearsFromNow.getFullYear() + 2);
+
+
+    const pastCount = Math.round(count * 0.33);
+    const nearFutureCount = Math.round(count * 0.33);
+    const farFutureCount = count - pastCount - nearFutureCount;
+
+
+    // 33% - sometime during the previous two years
+    for (let i = 0; i < pastCount; i++) {
+        dates.push(
+            randomDateBetween(twoYearsAgo, yesterday)
+        );
+    }
+
+
+    // 33% - sometime during the next 60 days
+    for (let i = 0; i < nearFutureCount; i++) {
+        dates.push(
+            randomDateBetween(tomorrow, sixtyDaysFromNow)
+        );
+    }
+
+
+    // Remaining dates - more than 60 days away,
+    // but less than two years away
+    for (let i = 0; i < farFutureCount; i++) {
+        dates.push(
+            randomDateBetween(sixtyOneDaysFromNow, twoYearsFromNow)
+        );
+    }
+
+
+    // Shuffle the array so the categories aren't grouped together
+    for (let i = dates.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [dates[i], dates[j]] = [dates[j], dates[i]];
+    }
+
+    return dates;
+}
+
