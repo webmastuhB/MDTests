1a6cced3e35280858f0a471b11cb746089cfdadf
WebmastuhB <brian1701a@gmail.com>
2026-09-10 22:54:58 -0500

    Badge

--- Boat3/BoatCard.vue
+++ Boat3/BoatCard.vue
@@ -5,6 +5,46 @@ const props = defineProps({
         required: true
     }
 });
+
+const expirationStatus = computed(() => {
+
+    if (!props.boat?.expDate) {
+        return null;
+    }
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const expDate = new Date(props.boat.expDate);
+
+    if (Number.isNaN(expDate.getTime())) {
+        return null;
+    }
+
+    expDate.setHours(0, 0, 0, 0);
+
+    const sixtyDaysFromNow = new Date(today);
+    sixtyDaysFromNow.setDate(
+        sixtyDaysFromNow.getDate() + 60
+    );
+
+
+    if (expDate < today) {
+        return {
+            text: "Expired",
+            className: "boat-status-expired"
+        };
+    }
+
+    if (expDate <= sixtyDaysFromNow) {
+        return {
+            text: "Almost Expired",
+            className: "boat-status-almost-expired"
+        };
+    }
+
+    return null;
+});
 </script>
 
 <template>
